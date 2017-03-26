81ab60e8afa4fd2b08101f36839c5973e350d596
Oliver Gilan <gilano@st.northernhighlands.org>
2017-03-26 13:15:24 -0400

    Project
    This is the whole project

---
+++ Ster Wers/Assets/Scripts/EnemyBullet.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This class is for the green lasers that are fired from enemy TIE fighters
+
+public class EnemyBullet : MonoBehaviour {
+
+    private GameController gameController;
+    public AudioSource enemyFireSound;
+    public GameObject player;
+    public GameObject explosion;
+
+    private Vector3 spawn;
+    private float startTime;
+    private float speed = .3f;
+    private Vector3 target;
+
+	// Use this for initialization
+	void Start () {
+        gameController = FindObjectOfType<GameController>();
+        spawn = transform.position;
+        startTime = Time.time;
+        target = player.transform.position;
+        //enemyFireSound.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //transform.position = Vector3.Lerp(spawn, target, (Time.time - startTime) / speed);
+        transform.position = Vector3.MoveTowards(transform.position, target, (Time.time - startTime)/ speed);
+        if(transform.position == target)
+        {
+            //Creates probability of bullet harming player
+            int hit = Random.Range(0, 15);
+            if(hit == 1)
+            {
+                Vector3 player = new Vector3(3.5f, .6f, -4.02f);
+                gameController.gotHit();
+                GameObject expl = Instantiate(explosion, player, transform.rotation) as GameObject;
+                Destroy(explosion, 3);
+            }
+            Destroy(gameObject);
+        }
+	}
+
+    
+}
---
+++ Ster Wers/Assets/Scripts/Shooting.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Controls the player's shooting mechanics
+public class Shooting : MonoBehaviour {
+
+    public Rigidbody laserPrefab;
+    public Transform rightBarrel;
+    public Transform leftBarrel;
+    public AudioSource fireSound;
+
+    private GameController gameController;
+
+	// Use this for initialization
+	void Start () {
+        gameController = FindObjectOfType<GameController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (GvrViewer.Instance.VRModeEnabled && GvrViewer.Instance.Triggered && !gameController.isGameOver)
+        {
+            Rigidbody rightLaserInstance;
+            rightLaserInstance = Instantiate(laserPrefab, rightBarrel.position, rightBarrel.rotation) as Rigidbody;
+            rightLaserInstance.AddForce(rightBarrel.up * 3000);
+            Rigidbody leftLaserInstance;
+            leftLaserInstance = Instantiate(laserPrefab, leftBarrel.position, leftBarrel.rotation) as Rigidbody;
+            leftLaserInstance.AddForce(leftBarrel.up * 3000);
+            fireSound.Play();
+        }
+        //For debugging on pc
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Rigidbody rightLaserInstance;
+            rightLaserInstance = Instantiate(laserPrefab, rightBarrel.position, rightBarrel.rotation) as Rigidbody;
+            rightLaserInstance.AddForce(rightBarrel.up * 3000);
+            Rigidbody leftLaserInstance;
+            leftLaserInstance = Instantiate(laserPrefab, leftBarrel.position, leftBarrel.rotation) as Rigidbody;
+            leftLaserInstance.AddForce(leftBarrel.up * 3000);
+            fireSound.Play();
+        }
+    }
+}
